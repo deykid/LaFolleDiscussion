7d02aeb4a6303026ad4dbf72c6b8b035f719d688
Deykid <33992397+deykid@users.noreply.github.com>
2020-10-19 10:32:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+var socket;
+var usernameInput
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var dingSound;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
+  chatRoom = document.getElementById("RoomID");
+  dingSound = document.getElementById("Ding");
+
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  })
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 9){
+      messages.push(message);
+      dingSound.currentTime = 0;
+      dingSound.play();
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("join", "Room1","DD");//chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 1000);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+
+function delayReset(){
+  delay = true;
+}
+
+document.querySelector('.chat[data-chat=person2]').classList.add('active-chat')
+document.querySelector('.person[data-chat=person2]').classList.add('active')
+
+let friends = {
+    list: document.querySelector('ul.people'),
+    all: document.querySelectorAll('.left .person'),
+    name: ''
+  },
+  chat = {
+    container: document.querySelector('.container .right'),
+    current: null,
+    person: null,
+    name: document.querySelector('.container .right .top .name')
+  }
+
+friends.all.forEach(f => {
+  f.addEventListener('mousedown', () => {
+    f.classList.contains('active') || setAciveChat(f)
+  })
+});
+
+function setAciveChat(f) {
+  friends.list.querySelector('.active').classList.remove('active')
+  f.classList.add('active')
+  chat.current = chat.container.querySelector('.active-chat')
+  chat.person = f.getAttribute('data-chat')
+  chat.current.classList.remove('active-chat')
+  chat.container.querySelector('[data-chat="' + chat.person + '"]').classList.add('active-chat')
+  friends.name = f.querySelector('.name').innerText
+  chat.name.innerHTML = friends.name
+}
